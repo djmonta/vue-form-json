c6b4cd21f706f7f6248c1152a75eaa146c53cb3f
monta <djmonta@gmail.com>
2019-07-02 22:20:20 +0900

    change error message locale to japanese

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
 import App from './App.vue'
+import japanese from 'vee-validate/dist/locale/ja'
 
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, { locale: 'ja' })
+Validator.localize('ja', japanese)
+
 Vue.config.productionTip = false
 
 new Vue({
